c851e295fa9f3b2044fb558e096014892b4673b0
vidhishaprabhu <vidhishaprabhu@gmail.com>
2026-03-12 10:17:43 +0530

    Resolved the error caused be cors

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const dotenv=require('dotenv')
 dotenv.config({path:'./config/.env'})
 const connectDB=require('./config/db');
 const morgan = require('morgan');
+const cors=require('cors');
+const router=require('./routes/employee');
 connectDB()
 
 app.use(express.json());
 app.use(morgan('dev'))
+app.use(cors())
 
-const router=require('./routes/employee');
 app.use('/api/employee',router);
+
+
 app.listen(process.env.PORT,()=>{
   try{
     console.log(`Server started at ${process.env.PORT}`)
